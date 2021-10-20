1438a8ce4502b959a57f4cb52651bb19ce0e7637
Daniel Read0nly <ihatemat@yandex.ru>
2021-10-20 18:34:24 +0500

    redux removed

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { store } from "./app/store";
-import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import "./styles.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
